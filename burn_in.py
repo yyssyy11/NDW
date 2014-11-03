9631e20075af5df3732bf59e2aa1502bc541c3e0
yyssyy11 <yyssyy11@gmail.com>
2014-11-03 11:40:29 +0800

    批量烧写脚本

--- burn_in.py
+++ burn_in.py
@@ -7,9 +7,9 @@ import os
 burn_1 = 'make telosb install.'
 burn_2 = ' bsl,/dev/ttyUSB'
 
-for i in range(0, 27):
+for i in range(0, 4):
 	NODEid = str(i)
-	USBid = str(i+10)
+	USBid = str(i)
 	burn = burn_1 + NODEid + burn_2 + USBid
 	os.system(burn)
 	print '-----------------------\n' + 'NODE %d '%(i) + 'burned!!\n' + '----------------------'
